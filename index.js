1295ba5a330325ead51a38efd9156e644f6fb9e4
Telvin Varfley <telvinvarfley@gmail.com>
2023-02-20 14:32:32 +0000

    Setting everything up for deployment

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const connect = ()=>{
 
 
 // Server opened
-const port = process.env.port ||3000
+const port = process.env.PORT ||3000
 app.listen(port, ()=>{
     connect()
     console.log("Connected")
